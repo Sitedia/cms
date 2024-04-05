595738c0b7f3720666334541821efc57ec0290a8
cedric <cedric@localhost>
2024-04-05 07:29:39 +0200

    Clean project

--- apps/backend-e2e/src/support/global-teardown.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable unicorn/prefer-module */
-/* eslint-disable unicorn/no-anonymous-default-export */
-/* eslint-disable no-console */
-module.exports = async function () {
-  // Put clean up logic here (e.g. stopping services, docker-compose, etc.).
-  // Hint: `globalThis` is shared between setup and teardown.
-  console.log(globalThis.__TEARDOWN_MESSAGE__);
-};
