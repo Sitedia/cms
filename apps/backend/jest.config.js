8f77e4cffcb74e1498cb3c548a40b94a7c1e41ab
cedric <cedric@localhost>
2024-04-05 13:20:51 +0200

    Simplify configurations

--- apps/backend/jest.config.js
+++ apps/backend/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   displayName: 'nestjs-common',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
   },
   coverageDirectory: './coverage',
 };
