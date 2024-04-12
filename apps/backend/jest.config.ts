81ed630d804a392939962b80000c1493b04259df
cedric <cedric@localhost>
2024-04-12 15:47:26 +0200

    Update configuration

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -6,7 +6,7 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json', diagnostics: false }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  setupFilesAfterEnv: [__dirname + '/dotenv.config.js'],
+  setupFilesAfterEnv: ['<rootDir>/dotenv.config.js'],
   coverageDirectory: './coverage',
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.test.ts', '!src/index.ts', '!src/main.ts'],
 };
