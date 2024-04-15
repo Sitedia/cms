1635eb78f8a77d6bdd463e3f18bcfd1f098dcd5c
cedric <cedric@localhost>
2024-04-15 17:57:22 +0200

    Update tests configuration

--- jest.preset.js
+++ jest.preset.js
@@ -3,7 +3,5 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'mjs', 'html'],
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: ['src/**/*.ts', '!src/*.ts'],
-  transform: { '^.+\\.(ts|js|html)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json', diagnostics: false }] },
-  testEnvironment: 'node',
-  testEnvironmentOptions: { customExportConditions: ['node', 'require', 'default'] },
+  transform: { '^.+\\.(ts|js)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json', diagnostics: false }] },
 };
