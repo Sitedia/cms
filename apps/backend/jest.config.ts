4f1883a4008de295596aa19bfc809987e4614bca
cedric <cedric@localhost>
2024-04-15 18:35:11 +0200

    Simplify configuration

--- apps/backend/jest.config.ts
+++ apps/backend/jest.config.ts
@@ -1,6 +1,5 @@
 export default {
   preset: '../../jest.preset.js',
   displayName: 'backend',
-  modulePathIgnorePatterns: ['<rootDir>/dist'],
   setupFilesAfterEnv: ['<rootDir>/dotenv.config.js'],
 };
--- jest.preset.js
+++ jest.preset.js
@@ -1,6 +1,7 @@
 module.exports = {
   resolver: '@nx/jest/plugins/resolver',
   moduleFileExtensions: ['ts', 'js', 'mjs', 'html'],
+  modulePathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/coverage'],
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: ['src/**/*.ts', '!src/*.ts'],
   transform: { '^.+\\.(ts|js)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json', diagnostics: false }] },
