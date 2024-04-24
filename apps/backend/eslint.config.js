cc22488a389da52c7dffeaab535962340c6c752f
cedric <cedric@localhost>
2024-04-24 22:14:58 +0200

    Solve issue

--- apps/backend/eslint.config.js
+++ apps/backend/eslint.config.js
@@ -1,6 +1,9 @@
+import eslint from '@eslint/js';
+import nxEslintPlugin from '@nx/eslint-plugin';
+import jestPlugin from 'eslint-plugin-jest';
+import unicornPlugin from 'eslint-plugin-unicorn';
 import jsoncParser from 'jsonc-eslint-parser';
 import tseslint from 'typescript-eslint';
-import preset from '../../eslint.base.js';
 
 export default [
   {
@@ -23,5 +26,50 @@ export default [
       },
     },
   },
-  ...preset,
+  { plugins: { '@nx': nxEslintPlugin } },
+  {
+    ignores: ['dist', 'coverage', '*.config.js'],
+  },
+  eslint.configs.recommended,
+  jestPlugin.configs['flat/all'],
+  unicornPlugin.configs['flat/all'],
+  {
+    rules: {
+      camelcase: 'error',
+      curly: 'error',
+      eqeqeq: 'error',
+      complexity: ['error', 20],
+      'max-depth': ['error', 4],
+      'max-params': ['error', 8],
+      'no-console': 'warn',
+      'no-magic-numbers': ['error', { ignore: [0, 1] }],
+      radix: 'error',
+      '@nx/enforce-module-boundaries': ['error'],
+      'unicorn/prevent-abbreviations': ['error', { ignore: ['app', 'e2e', 'props', 'moduleRef'] }],
+      'jest/require-hook': 'off',
+      'jest/unbound-method': 'off',
+    },
+  },
+  ...tseslint.configs.recommendedTypeChecked.map((conf) => ({ ...conf, files: ['**/*.ts'] })),
+  {
+    files: ['**/*.spec.ts'],
+    rules: { 'jest/require-hook': ['error'] },
+  },
+  {
+    files: ['**/*.spec.ts', '**/*.dto.ts', '**/*.entity.ts'],
+    rules: { 'no-magic-numbers': 'off' },
+  },
+  {
+    files: ['**/*.json'],
+    rules: {
+      'jest/unbound-method': 'off',
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredFiles: ['**/*.spec.ts', '**/*.js'],
+          includeTransitiveDependencies: true,
+        },
+      ],
+    },
+  },
 ];
