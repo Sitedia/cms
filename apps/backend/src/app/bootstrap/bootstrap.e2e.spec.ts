cb5a7bffc5c0641df6848930249662814d919b5f
cedric <cedric@localhost>
2024-04-28 15:01:53 +0200

    Update configuration

--- apps/backend/src/app/bootstrap/bootstrap.e2e.spec.ts
+++ apps/backend/src/app/bootstrap/bootstrap.e2e.spec.ts
@@ -1,4 +1,3 @@
-import { LogFormat } from '#libs/common';
 import { describe, expect, it } from '@jest/globals';
 import { HttpStatus } from '@nestjs/common';
 import * as http from 'node:http';
@@ -6,51 +5,26 @@ import * as request from 'supertest';
 import { bootstrap } from './bootstrap.js';
 
 describe('nestjs application', () => {
-  it('should display the status of the application', async () => {
-    expect.assertions(2);
+  it('should display start the application', async () => {
+    expect.assertions(1);
     process.env.PORT = '3001';
-    process.env.APP_TLS_ENABLED = 'false';
-    process.env.APP_SWAGGER_UI_ENABLED = 'true';
     process.env.APP_LOG_ENABLED = 'false';
-    process.env.APP_LOG_FORMAT = 'CONSOLE';
+    process.env.APP_TLS_ENABLED = 'false';
+    expect.assertions(1);
     const application = await bootstrap();
     const httpServer = application.getHttpServer() as http.Server;
     const response = await request.agent(httpServer).get('/api/probes/liveness');
 
     expect(response.statusCode).toBe(HttpStatus.OK);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    expect(response.body.status).toBe('ok');
-
-    await application.close();
-  });
-
-  it('should be able to start on a specific port', async () => {
-    expect.assertions(2);
-    process.env.PORT = '3002';
-    process.env.APP_TLS_ENABLED = 'false';
-    process.env.APP_SWAGGER_UI_ENABLED = 'true';
-    process.env.APP_LOG_ENABLED = 'false';
-    process.env.APP_LOG_FORMAT = LogFormat.CONSOLE;
-    process.env.DEFAULT_STORAGE_THRESHOLD = '1';
-    const application = await bootstrap();
-    const httpServer = application.getHttpServer() as http.Server;
-    const response = await request.agent(httpServer).get('/api/probes/liveness');
-
-    expect(response.statusCode).toBe(HttpStatus.OK);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    expect(response.body.status).toBe('ok');
 
     await application.close();
   });
 
   it('should start in production environment using HTTPs', async () => {
     expect.assertions(1);
-    process.env.PORT = '3002';
-    process.env.APP_TLS_ENABLED = 'true';
-    process.env.APP_SWAGGER_UI_ENABLED = 'false';
+    process.env.PORT = '3001';
     process.env.APP_LOG_ENABLED = 'false';
-    process.env.APP_LOG_FORMAT = 'JSON';
-    process.env.DEFAULT_STORAGE_THRESHOLD = '1';
+    process.env.APP_TLS_ENABLED = 'true';
     const application = await bootstrap();
     const httpServer = application.getHttpServer() as http.Server;
 
@@ -61,11 +35,9 @@ describe('nestjs application', () => {
 
   it('should log invalid requests', async () => {
     expect.assertions(1);
+    process.env.PORT = '3001';
     process.env.APP_TLS_ENABLED = 'false';
     process.env.APP_LOG_ENABLED = 'false';
-    process.env.APP_LOG_FORMAT = LogFormat.CONSOLE;
-    process.env.DEFAULT_STORAGE_THRESHOLD = '1';
-    delete process.env.APP_SWAGGER_UI_ENABLED;
     const application = await bootstrap();
     const httpServer = application.getHttpServer() as http.Server;
     const response = await request.agent(httpServer).get('/unknown-path');
@@ -77,10 +49,9 @@ describe('nestjs application', () => {
 
   it('should log internal errors', async () => {
     expect.assertions(1);
+    process.env.PORT = '3001';
     process.env.APP_TLS_ENABLED = 'false';
     process.env.APP_LOG_ENABLED = 'false';
-    process.env.APP_LOG_FORMAT = LogFormat.CONSOLE;
-    process.env.APP_SWAGGER_UI_ENABLED = 'INVALID';
     process.env.APP_STORAGE_THRESHOLD = '0';
     const application = await bootstrap();
     const httpServer = application.getHttpServer() as http.Server;
