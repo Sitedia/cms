d31b01436bc7c5a723759ec4a3199032bfdd61f9
cedric <cedric@localhost>
2024-05-02 21:14:24 +0000

    Clean

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,13 +1,15 @@
 echo
+echo -e "\033[1;32mConfigure Npm ...\033[0m"
+sudo npm config set fund false
+sudo npm install -g npm
+
+echo
 echo -e "\033[1;32mPrepare node_modules folder ...\033[0m"
 sudo chown node node_modules
 
 echo
-echo -e "\033[1;32mUpdate Npm ...\033[0m"
-npm install -g npm
-
-echo
 echo -e "\033[1;32mInstall Nx 18 globally ...\033[0m"
+npm config set fund false
 npm install -g nx@^18
 
 echo
