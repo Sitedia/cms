4b1a4d801abe9b45fac6f44173a123f43d46d304
cedric <cedric@localhost>
2024-05-03 18:29:32 +0000

    Fix debuggger

--- libs/common/src/lib/health/health.controller.ts
+++ libs/common/src/lib/health/health.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
 import { ApplicationLogger } from '../logger/application.logger.js';
-//
+
 @ApiTags('probes')
 @Controller('probes')
 export class HealthController {
