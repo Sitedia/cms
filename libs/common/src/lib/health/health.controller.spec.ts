46e5f6bf49cd8d268b7167d9ed0dc2da2e15aec9
cedric <cedric@localhost>
2024-05-08 06:19:12 +0000

    Update health test

--- libs/common/src/lib/health/health.controller.spec.ts
+++ libs/common/src/lib/health/health.controller.spec.ts
@@ -1,37 +1,31 @@
 import { describe, expect, it } from '@jest/globals';
-import { HttpStatus, ModuleMetadata } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import * as http from 'node:http';
-import * as request from 'supertest';
 import { LoggerModule } from '../logger/logger.module.js';
+import { HealthController } from './health.controller.js';
 import { HealthModule } from './health.module.js';
 
 const setup = async () => {
-  const metadata: ModuleMetadata = { imports: [HealthModule, LoggerModule.register({ level: 'fatal' })] };
-  const module: TestingModule = await Test.createTestingModule(metadata).compile();
-  return await module.createNestApplication().init();
+  const app: TestingModule = await Test.createTestingModule({
+    imports: [HealthModule, LoggerModule.register({ level: 'fatal' })],
+  }).compile();
+
+  return app.get(HealthController);
 };
 
-describe('health controller', () => {
+describe('health endpoint', () => {
   it('should return the liveness status', async () => {
     expect.assertions(1);
-    const app = await setup();
-    const httpServer = app.getHttpServer() as http.Server;
+    const healthController = await setup();
+    const healthCheckStatus = await healthController.liveness();
 
-    const livenessResponse = await request.agent(httpServer).get('/probes/liveness');
-    expect(livenessResponse.statusCode).toBe(HttpStatus.OK);
-
-    await app.close();
+    expect(healthCheckStatus.status).toBe('OK');
   });
 
   it('should return the readiness status', async () => {
     expect.assertions(1);
-    const app = await setup();
-    const httpServer = app.getHttpServer() as http.Server;
+    const healthController = await setup();
+    const healthCheckStatus = await healthController.readiness();
 
-    const livenessResponse = await request.agent(httpServer).get('/probes/readiness');
-    expect(livenessResponse.statusCode).toBe(HttpStatus.OK);
-
-    await app.close();
+    expect(healthCheckStatus.status).toBe('OK');
   });
 });
